5b8797af442c16d8b4fe987311a239f48da50e58
Neil Kodner <nkodner@gmail.com>
2011-05-12 14:43:45 -0400

    updated scale

--- utah_pollen.py
+++ utah_pollen.py
@@ -1,17 +1,35 @@
+#!/usr/bin/python
+
+import urllib2, time
 import tweepy
 from BeautifulSoup import BeautifulSoup
-import urllib2
+
+TWITTER_ACCESS_KEY = '205504973-zS8dx6D5awBAXVGWuoJjKEMP070mb1oUfSTiyclL'
+TWITTER_ACCESS_SECRET = 'QBgg9v7jdSTJORRRHpB7vZBpxAFGGbX8Nn13GqzJs0'
+
+scale = {40: 'extra low', 80: 'low', 160: 'moderate', 400 : 'extra high'}
 
 def get_pollen_count():
+	datafile = 'data/utahpollen.tsv'
+	file_handle = open(datafile, 'a')
+	
 	page = urllib2.urlopen('http://intermountainallergy.com/pollen.html')
 	soup = BeautifulSoup(page)
 	pollentable = soup.find('table',{'class':'pollentable'})
 	rows = pollentable.findAll('tr')
+
+	today_str = time.strftime('%Y-%m-%d',time.localtime())
+
 	for row in rows[1:-2]:
 		cols = row.findAll('td')
 		pollen_type = cols[0].text
 		width = dict(cols[1].img.attrs)['width']
-		print "%s\t%s" % (pollen_type, width)
+		result =  "%s\t%s\t%s" % (today_str,pollen_type, width)
+		file_handle.write(result + '\n')
+		print result
+	
+	file_handle .close()
+
 
 def main():
 	get_pollen_count()
