710c47da4437ce9374e85a376087a7a3f1ac5e2a
neilkod <nkodner@gmail.com>
2011-06-23 22:40:38 -0400

    replaced beautifulsoup with pyquery due to some bad source html

--- utah_pollen.py
+++ utah_pollen.py
@@ -4,7 +4,8 @@ import csv, ConfigParser
 import numpy
 import urllib2, time, datetime
 import tweepy
-from BeautifulSoup import BeautifulSoup
+#from BeautifulSoup import BeautifulSoup
+from pyquery import PyQuery
 
 CONFIG_FILE = 'utah_pollen.cfg'
 TWITTER_SCREEN_NAME = 'neilkod2'
@@ -31,31 +32,48 @@ def get_twitter_config(config_file = CONFIG_FILE, screen_name = 'neilkod2'):
 	return twitter_params
 
 def get_pollen_count():
+	SOURCE_URL = 'http://intermountainallergy.com/pollen.html'
 	pollen_data = {}
-	page = urllib2.urlopen('http://intermountainallergy.com/pollen.html')
+	source = urllib2.urlopen(SOURCE_URL)
+
 	
 	# retrieve the last-modified http header
 	# convert it to a date
 	# and write it to a file
 	
-	lm = page.headers['last-modified']
+	lm = source.headers['last-modified']
 	last_modified_date = datetime.datetime.strptime(lm,'%a, %d %b %Y %H:%M:%S %Z')
 	# save the last-modified date
 	last_modified_file = open('last_modified.txt','wb')
 	last_modified_file.write(lm + '\n')
 	last_modified_file.close()
+
+	# 23-jun-2011 commenting out BeautifulSoup Code, replacing
+	# with newly-learned pyquery.
+	# disclaimer - I know next to nothing about jquery so my
+	# pyquery won't be the best.
 	
-	soup = BeautifulSoup(page)
-	pollentable = soup.find('table',{'class':'pollentable'})
-	rows = pollentable.findAll('tr')
-	print rows
-	for row in rows[1:-2]:
-		cols = row.findAll('td')
-		pollen_type = cols[0].text.replace(' ','')
-		width = dict(cols[1].img.attrs)['width']
-		result =  "%s\t%s\t%s" % (today_str,pollen_type, width)
-		print result
-		pollen_data[pollen_type] = int(width)
+	# soup = BeautifulSoup(page)
+	# pollentable = soup.find('table',{'class':'pollentable'})
+	# rows = pollentable.findAll('tr')
+	# print rows
+	# for row in rows[1:-2]:
+	# 	cols = row.findAll('td')
+	# 	pollen_type = cols[0].text.replace(' ','')
+	# 	width = dict(cols[1].img.attrs)['width']
+	# 	result =  "%s\t%s\t%s" % (today_str,pollen_type, width)
+	# 	print result
+	# 	pollen_data[pollen_type] = int(width)
+
+	# create the pyquery object
+	d = PyQuery(source.read())
+	pollen_table = d('table.pollentable')
+	pollen_rows = pollen_table('tr')[1:-1]
+	for row in pollen_rows:
+		fields = row.getchildren()
+		pollen_source = fields[0].text_content()
+		pollen_value = fields[1].getchildren()[0].get('width')
+		pollen_data[pollen_source] = int(pollen_value)
 	
 	print pollen_data
 	return pollen_data
@@ -112,12 +130,12 @@ def main():
 
 	pollen_data = get_pollen_count()
 #	pollen_data = TESTDATA
-#	write_pollen_data(pollen_data)
+	write_pollen_data(pollen_data)
 	tweet_string = report_pollen_data(pollen_data)
 	print tweet_string
-	# success = send_tweet(tweet_string)
-	# if success:
-	# 	print "tweet successfully sent: %s"  % tweet_string
+	success = send_tweet(tweet_string)
+	if success:
+		print "tweet successfully sent: %s"  % tweet_string
 
 if __name__ == '__main__':
 	main()
